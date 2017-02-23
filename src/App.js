f1734782258f30c944d9422d0996368fbed08509
cjredmond <connor.redmond@gmail.com>
2017-02-23 11:16:47 -0500

    can access api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      people: []
+    }
+  }
+
+  getPeople(){
+    $.ajax({
+      url: 'https://fierce-mountain-11501.herokuapp.com/api/:persons/',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({people: data}, function(){
+          console.log(this.state)
+        }
+        );
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+      }
+    })
+  }
+
+  componentWillMount(){
+    this.getPeople();
+  }
+
+  
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
